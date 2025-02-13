abf0c48585d990c37b1e288ba51969e2a6206de6
mazensabry2712 <mazensoftware30@gmail.com>
2025-02-14 00:42:18 +0200

    Settings

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Setting;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot()
+    {
+        if(!app()->runningInConsole()){
+        $setting = Setting::firstOr(function () {
+            return Setting::create([
+                 'name' => 'site_name',
+                 'description' => 'Laravel'
+             ]);
+          });
+          view()->share('setting', $setting);
+        }
+    }
+}
